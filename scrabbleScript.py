e2abbae4ec3a457adf3565140e1c0206e1c5060f
twiby <61565340+twiby@users.noreply.github.com>
2020-02-27 23:28:33 +0100

    Add files via upload

---
+++ scrabbleScript.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+### load other file
+class add_path():
+    def __init__(self, path):
+        self.path = path
+
+    def __enter__(self):
+        for i in range(len(self.path)):
+            sys.path.insert(0, self.path[i])
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        for i in range(len(self.path)):
+            sys.path.remove(self.path[i])
+
+folder = os.path.dirname(os.path.realpath(__file__)) + "/"
+python_sys_path = [folder[:-1]]
+with add_path(python_sys_path):
+    utils = __import__("scrabbleTreeUtils")
+
+
+### Get all the words of scrabble in a text file
+if not os.path.isfile(folder + "scrabbleWords.txt"):
+	utils.writeScrabbleWordsToNewFile(folder + "scrabbleWords.txt")
+
+### Organize the data in a tree
+if not os.path.isfile(folder + "scrabble.tree"):
+	scrabbleTree = utils.createTree(folder + "scrabbleWords.txt")
+	scrabbleTree.save(folder + "scrabble.tree")
+else:
+	scrabbleTree = utils.loadTree(folder + "scrabble.tree")
+
+# scrabbleTree.children[0].children[-1].print()
+# scrabbleTree.children[0].children[-1].children[0].print()
+# print(scrabbleTree.getWord("azure").asString())
+
+
+
+print({x.asString() for x in scrabbleTree.getAllAnagrams("apporte")})
---
+++ scrabbleTreeUtils.py
@@ -0,0 +1,229 @@
+from lxml import html
+import requests
+import sys
+
+
+
+### Utils to get words from internet
+def replaceWeirdC(string):
+	for i in range(len(string)):
+		if string[i]=="ç":
+			string = string[:i]+"%C3%A7"+string[i+1:]
+	return string
+
+
+def writeScrabbleWordsToNewFile(path):
+	homePageLink = "http://scrabblemania.fr"
+	page = requests.get(homePageLink+"/tous-les-mots")
+	tree = html.fromstring(page.content)
+
+	allLinks = tree.xpath('//a')
+	selectedLinks = []
+	for link in allLinks:
+		string = link.attrib['href']
+		if string[:9]=="/mots-de-" and string[-8:]!="scrabble":
+			selectedLinks.append(homePageLink+ replaceWeirdC(string))
+		else:
+			continue
+
+
+	chars = set('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ/ \\')
+	with open(path, 'w') as f:
+
+		for link in selectedLinks:
+			sys.stdout.write(link+"    ")
+			sys.stdout.write('\r')
+			sys.stdout.flush()
+
+			page = requests.get(link)
+			tree = html.fromstring(page.content)
+			textContents = tree.xpath('//a/text()')
+			for txt in textContents:
+				if any((c in chars) for c in txt) or txt=="Scrabblemania":
+					continue
+				f.write("%s\n" % txt)
+	print()
+
+
+
+
+
+
+
+
+
+from bisect import bisect_left
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    def word(word, indent=""):
+    	if word.isWord:
+    		str = bcolors.OKGREEN
+    	else:
+    		str = bcolors.WARNING
+    	str += indent + word.data + bcolors.ENDC
+    	return str
+
+class KeyifyList(object):
+	'''wrapper class to be able to use bisort functions with key functions'''
+	def __init__(self, inner, key):
+	    self.inner = inner
+	    self.key = key
+
+	def __len__(self):
+		return len(self.inner)
+
+	def __getitem__(self, k):
+		return self.key(self.inner[k])
+
+
+
+
+
+
+
+###################################################################################################################
+
+# MAIN CLASS FOR SCRABBLE DATA BASE
+
+class TreeNode(object):
+	'''Recursive data structure. Stores words letter by letter as a tree structure.
+	All methods are meant to be called from top tree. They are not called from subnodes other than for recursion (except print for debug reasons).'''
+	def __init__(self, data="", isWord=True):
+		self.data = data
+		self.isWord = isWord
+		self.children = []
+		self.parent = None
+
+
+
+
+	def addChild(self, child):
+		idx = bisect_left(KeyifyList(self.children, lambda c: c.data), child.data)
+		child.parent = self
+		self.children.insert(idx, child)
+	def addNewChild(self, *args, **kwargs):
+		temp = TreeNode(*args, **kwargs)
+		self.addChild(temp)
+		return temp
+
+	def addWord(self, word):
+		if len(word) == 1:
+			self.addNewChild(word, isWord=True)
+			return
+		nextBranch = self.getNextBranch(word[0])
+		if nextBranch==None:
+			nextBranch = self.addNewChild(word[0], isWord=False)
+		nextBranch.addWord(word[1:])
+
+	def getNextBranch(self, c):
+		idx = bisect_left(KeyifyList(self.children, lambda c: c.data), c)
+		if idx>=len(self.children) or c != self.children[idx].data:
+			return None
+		else:
+			return self.children[idx]
+
+	def getNode(self, word):
+		nb = self.getNextBranch(word[0])
+		if len(word)==1 or nb==None:
+			return nb
+		else:
+			return nb.getNode(word[1:])	
+	def getWord(self, word):
+		'''same as getNode but returns None if the node is not a word'''
+		node = self.getNode(word)
+		if node==None or not node.isWord:
+			return None
+		else:
+			return node
+
+	### Scrabble methodes ###
+	### The variable 'set' stands for the set of letters that the player wants to play, plus maybe other letters from the board.
+
+	def getAllPermutations(self, set, prefix=[]):
+		'''Not used in practice (because we use a tree structure). We keep it here for reference'''
+		myList = list(set)
+		if len(myList)==1:
+			yield prefix + myList
+		else:
+			for idx in range(len(myList)):
+				yield from self.getAllPermutations(myList[:idx]+myList[idx+1:], prefix=prefix+[myList[idx]])
+
+	def getAllAnagrams(self, set):
+		setList = list(set)
+		if len(setList)==0:
+			yield self
+		for idx in range(len(setList)):
+			node = self.getNextBranch(setList[idx])
+			if node==None:
+				continue
+			if len(setList)==1 and not node.isWord:
+				continue
+			yield from node.getAllAnagrams(setList[:idx]+setList[idx+1:])
+
+	### End of scrabble methods ###
+
+
+
+	### Utils
+	def asString(self):
+		prefix=""
+		node = self.parent
+		while not node==None:
+			prefix = node.data + prefix
+			node = node.parent
+		return (prefix + self.data)
+	def print(self, indent="", onlyWords=False, getParent=True):
+		### if first call of recursion, get previous letters in tree
+		if getParent==True:
+			node = self.parent
+			while not node==None:
+				indent = node.data + indent
+				node = node.parent
+		### print current node
+		if not onlyWords or self.isWord:
+			print(bcolors.word(self, indent=indent))
+		### print children nodes
+		for child in self.children:
+			child.print("  "+indent+self.data, onlyWords=onlyWords, getParent=False)
+	def save(self, path):
+		import pickle
+		print('saving word tree')
+		with open(path, "wb") as f:
+			pickle.dump(self, f)
+
+
+
+### END OF CLASS
+############################################################################################
+
+
+
+def createTree(path):
+	with open(path, 'r') as f:
+		words = f.read().splitlines()
+
+	scrabbleTree = TreeNode('', isWord=False)
+	for word in words:
+		sys.stdout.write(word + " "*10)
+		sys.stdout.write("\r")
+		sys.stdout.flush()
+		scrabbleTree.addWord(word)
+
+	print()
+	return scrabbleTree
+
+def loadTree(path):
+	import pickle
+	print("loading word tree")
+	with open(path, "rb") as f:
+		tree = pickle.load(f)
+	return tree
