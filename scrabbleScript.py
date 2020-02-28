d395076476dd0b1a4512cd9d27c1ae9cb57f4d7f
twiby <t.balenbois@gmail.com>
2020-02-28 11:37:50 +0100

    reorganization

--- scrabbleScript.py
+++ scrabbleScript.py
@@ -1,22 +1,23 @@
 import os
 import sys
 
-import scrabbleTreeUtils as utils
+import pyScrabble.scrabbleTree as ps
 
 folder = os.path.dirname(os.path.realpath(__file__)) + "/"
 
 
 ### Get all the words of scrabble in a text file
 if not os.path.isfile(folder + "scrabbleWords.txt"):
-	utils.writeScrabbleWordsToNewFile(folder + "scrabbleWords.txt")
+	from pyScrabble import scrabbleDict as sd
+	sd.writeScrabbleWordsToNewFile(folder + "scrabbleWords.txt")
 
 
 ### Organize the data in a tree
 if not os.path.isfile(folder + "scrabble.tree"):
-	scrabbleTree = utils.createTree(folder + "scrabbleWords.txt")
+	scrabbleTree = ps.createTree(folder + "scrabbleWords.txt")
 	scrabbleTree.save(folder + "scrabble.tree")
 else:
-	scrabbleTree = utils.loadTree(folder + "scrabble.tree")
+	scrabbleTree = ps.loadTree(folder + "scrabble.tree")
 
 # scrabbleTree.children[0].children[-1].print()
 # scrabbleTree.children[0].children[-1].children[0].print()
